80dcf64ed9656f1bb86983ca3f0e5d34722c6f3a
NathanHealea <nate.healea@gmail.com>
2016-02-17 21:07:22 -0800

    Update Game Class -- removed unnecessary spacing.

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -11,50 +11,17 @@ public class Game {
 
     public java.util.List<Card> deck = new ArrayList<>();
 
+
     public java.util.List<java.util.List<Card>> cols = new ArrayList<>();
 
 
     public Game(){
+        // Deck = englishDeck;
         cols.add(new ArrayList<Card>());
         cols.add(new ArrayList<Card>());
         cols.add(new ArrayList<Card>());
         cols.add(new ArrayList<Card>());
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //I like space between my methods
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     public void buildDeck() {
         for(int i = 2; i < 15; i++){
@@ -64,33 +31,6 @@ public class Game {
             deck.add(new Card(i,Suit.Spades));
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-    //I like space between my methods
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     public void shuffle() {
         long seed = System.nanoTime();
         Collections.shuffle(deck, new Random(seed));
