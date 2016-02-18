5bd4f56607784ebd7684bfb793103b3e4831e6e2
NathanHealea <nate.healea@gmail.com>
2016-02-17 20:59:46 -0800

    Update EsDeck -- Added build() Update Suit -- Addes suits for a spanish deck Updated testEnDeck -- remove unnecessary include Update testEsDeck -- added testDeckInitialization() -- added testBuildDeck() -- added testShuffleDeck() -- added testDrawCardFromDeck()

--- src/test/java/models/testEnDeck.java
+++ src/test/java/models/testEnDeck.java
@@ -1,6 +1,5 @@
 package models;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
--- src/test/java/models/testEsDeck.java
+++ src/test/java/models/testEsDeck.java
@@ -1,7 +1,69 @@
 package models;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by nathan on 2/17/16.
  */
 public class testEsDeck {
+
+    @Test
+    public void testDeckInitialization(){
+        // Arrange
+        Deck d = new EsDeck();
+
+        // Act
+
+        // Assert
+        assertNotEquals(null, d);
+
+    }
+
+    @Test
+    public void testBuildDeck(){
+        // Arrange
+        Deck d = new EsDeck();
+
+        // Act
+        d.build();
+
+        // Assert
+        assertNotEquals(null, d.deck);
+        assertEquals(40, d.deck.size());
+
+    }
+
+    @Test
+    public void testShuffleDeck(){
+        // Arrange
+        Deck unshuffled = new EsDeck();
+        Deck shuffeled = new EsDeck();
+
+        // Act
+        unshuffled.build();
+        shuffeled.build();
+        shuffeled.shuffle();
+
+        // Assert
+        assertNotEquals(unshuffled.deck, shuffeled.deck);
+
+    }
+
+    @Test
+    public void testDrawCardFromDeck(){
+        // Arrange
+        Deck d = new EsDeck();
+        Card cRec = null;
+        Card cExp = new Card(13,Suit.Cups);
+
+        // Act
+        d.build();
+        cRec = d.deal();
+
+        // Assert
+        assertNotEquals(null, cRec);
+        assertEquals(cExp.toString(),cRec.toString());
+    }
 }
